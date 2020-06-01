8506f44a450b2dae6babdc90e86cc3c61ea71a58
onion-rain <1934455602@qq.com>
2020-06-01 13:17:54 +0800

    生成目录part文件

---
+++ mk_catalogue.py
@@ -0,0 +1,16 @@
+import os
+
+root = "/home/xueruini/onion_rain/pytorch/dataset/thyroid/"
+
+imgs = os.listdir(root+"images")
+labels = os.listdir(root+"labels")
+
+with open("imgs_catalogue.part", "w+", encoding="utf-8") as f:
+    for path in imgs:
+        path = "/images/" + path + "\r\n"
+        f.write(path)
+
+with open("labels_catalogue.part", "w+", encoding="utf-8") as f:
+    for path in labels:
+        path = "/labels/" + path + "\r\n"
+        f.write(path)
